c3ef5a1154b31b041d9a1aa32d20d02759a6a50d
Amit Nambiar <amit.lzkpa@gmail.com>
2019-05-01 16:30:48 -0400

    track support data; small fixes

---
+++ support_data/curly/cmd.sh
@@ -0,0 +1,1 @@
+curl -X GET --header "Accept: application/json" --header "Authorization: Bearer cf9abb85d92fc7b061b8a4c936078e5a" "https://api.deutschebahn.com/fahrplan-plus/v1/departureBoard/8000105?date=2018-08-22T15%3A00"
